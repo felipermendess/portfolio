b66cc35ef271a6d5ddec5b860fdcc78b160ceb23
felipermendess <felipermendes04@gmail.com>
2024-08-05 18:33:59 -0400

    animacao ao scroll

--- script.js
+++ script.js
@@ -28,3 +28,60 @@ function ativeHandleClickBickraft(){
   linkBikcraft.addEventListener('click', handleClickBickcraft);
 }
 ativeHandleClickBickraft();
+
+function animacaoAoScrollFormacao(){
+  const formacao = document.querySelector('.formacao-conteudo');
+  
+    const windowMetade = window.innerHeight * 0.6;
+    function animaAoScrollFormacao(){
+      const formacaoTop = formacao.getBoundingClientRect().top;
+      const isFormacao = (formacaoTop - windowMetade) < 0;
+      if(isFormacao){
+        formacao.classList.add('ativo');
+      }
+    }
+    animaAoScrollFormacao();
+  
+    window.addEventListener('scroll', animaAoScrollFormacao);
+  
+}
+animacaoAoScrollFormacao();
+
+function animacaoAoScrollFooter(){
+  const footer = document.querySelector('.footer-conteudo');
+  
+    const windowMetade = window.innerHeight * 0.6;
+    function animaAoScrollFooter(){
+      const footerTop = footer.getBoundingClientRect().top;
+      const isFooter = (footerTop - windowMetade) < 0;
+      if(isFooter){
+        footer.classList.add('ativo');
+      }
+    }
+    animaAoScrollFooter();
+  
+    window.addEventListener('scroll', animaAoScrollFooter);
+}
+animacaoAoScrollFooter();
+
+function animacaoAoScroll(){
+    const sections = document.querySelectorAll('.scroll-js');
+    if(sections.length){
+      const windowMetade = window.innerHeight * 0.6;
+      function animaScroll(){
+        sections.forEach(section => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSection = (sectionTop - windowMetade) < 0;
+          if(isSection){
+            section.classList.add('ativo');
+          }
+        })
+      }
+      animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+
+    }
+    
+}
+animacaoAoScroll();
