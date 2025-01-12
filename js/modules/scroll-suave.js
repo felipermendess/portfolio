c21ae9018632b4c85cc3aaf06925b2cfadd51de1
felipermendess <felipermendes04@gmail.com>
2025-01-12 19:49:09 -0400

    ajustando function de scroll suave

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,23 +1,23 @@
 export default function scrollSuave(){
     const linksInternos = document.querySelectorAll('a[href^="#"]');
-    const events = ['click', 'touchstart'];
     
     function scrollToSection(event){
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
-        const topo = section.offsetTop;
-        window.scrollTo({
-            top: topo,
-            behavior: "smooth",
-        })
+        if(section){
+            const topo = section.offsetTop;
+            window.scrollTo({
+                top: topo,
+                behavior: "smooth",
+            })
+        }else{
+            console.log('Não foi possível encontrar nenhuma section!');
+        }
     }
     
     linksInternos.forEach(link => {
-        events.forEach(evento => {
-            link.addEventListener(evento, scrollToSection);
-        })
+        link.addEventListener('click', scrollToSection);
     })
-
 }
 
