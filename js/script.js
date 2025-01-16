c169ab6404f9304c145c5035ae24e47b2c846747
“Felipe <felipermendes04@gmail.com>
2025-01-16 17:50:39 -0400

    implementando function de animação ao scroll para testes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 import menuMobile from "./modules/menu-mobile.js";
 import scrollSuave from "./modules/scroll-suave.js";
+import animacaoAoScroll from "./modules/animacao-ao-scroll.js";
 
 menuMobile();
 scrollSuave();
+animacaoAoScroll();
 
