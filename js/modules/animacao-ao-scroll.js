c3b0a12d79c9a657bdf76ec877663c4e576bd96a
“Felipe <felipermendes04@gmail.com>
2025-01-17 17:09:54 -0400

    testes para função de animacao-ao-scroll

--- js/modules/animacao-ao-scroll.js
+++ js/modules/animacao-ao-scroll.js
@@ -1,7 +1,7 @@
 export default function animacaoAoScroll(){
     const sections = document.querySelectorAll('.js-scroll');
     if(sections.length){
-        const windowMetade = window.innerHeight * 0.8;
+        const windowMetade = window.innerHeight * 0.9;
         function animaScroll(){
             sections.forEach(section => {
                 const sectionTop = section.getBoundingClientRect().top;
