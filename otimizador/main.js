77e7744a6dc15d03f2a7f0f967171fbdd9bdbca8
felipermendess <felipermendes04@gmail.com>
2025-01-23 01:08:23 -0400

    adicionando babel

--- otimizador/main.js
+++ otimizador/main.js
@@ -1,36 +1,35 @@
 (() => {
   "use strict";
-  !(function () {
-    const e = document.querySelector(".buttonMenu"),
-      t = document.querySelector(".header-menu");
+  var e, t;
+  (e = document.querySelector(".buttonMenu")),
+    (t = document.querySelector(".header-menu")),
     e.addEventListener("click", function () {
       t.classList.contains("active")
         ? (t.classList.remove("active"),
           (document.querySelector(".icon").src = "img/buttonMenu.svg"))
         : (t.classList.add("active"),
           (document.querySelector(".icon").src = "img/closeMenu.svg"));
-    });
-  })(),
+    }),
     (function () {
       function e(e) {
         e.preventDefault();
-        const t = e.currentTarget.getAttribute("href");
+        var t = e.currentTarget.getAttribute("href");
         document
           .querySelector(t)
           .scrollIntoView({ behavior: "smooth", block: "start" });
       }
-      document.querySelectorAll('a[href^="#"]').forEach(t => {
+      document.querySelectorAll('a[href^="#"]').forEach(function (t) {
         t.addEventListener("click", e);
       });
     })(),
     (function () {
-      const e = document.querySelectorAll(".js-scroll"),
+      var e = document.querySelectorAll(".js-scroll"),
         t = 0.9 * window.innerHeight;
-      function c() {
-        e.forEach(e => {
+      function n() {
+        e.forEach(function (e) {
           e.getBoundingClientRect().top - t < 0 && e.classList.add("ativo");
         });
       }
-      e.length && (c(), window.addEventListener("scroll", c));
+      e.length && (n(), window.addEventListener("scroll", n));
     })();
 })();
